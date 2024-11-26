edd90d1dad59396dc811e563ca732a62766c0a4d
Rituraj Navindgikar <74851393+rituraj-navindgikar@users.noreply.github.com>
2024-11-26 10:36:16 -0500

    Time: 31 ms (5.02%) | Memory: 10.3 MB (16.15%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        char vowels[10] = {'a','e','i','o','u','A','E','I','O','U'};
+        int left = 0;
+        int right = s.size() - 1;
+
+        bool left_vowel = false, right_vowel = false;
+        int i = left;
+        int j = right/2;
+
+        while(left < right) {
+            if(check_vowel(s[left], vowels)){
+                left_vowel = true;
+            }else left++;
+            
+            if(check_vowel(s[right], vowels)) {
+                right_vowel = true;
+            }else right--;
+            
+            if(s[left] == s[right]) {
+                right--;
+                left++;
+                left_vowel = false;
+                right_vowel = false;
+            }
+
+            if(left_vowel && right_vowel) {     
+                cout << "found left " << s[left] << endl;     
+                cout << "found right " << s[right] << endl;
+
+                swap(s[left], s[right]);
+                cout << "swapping " << s[left] << " " << s[right] << endl;
+            
+                left_vowel = false;
+                right_vowel = false;
+                left++;
+                right--;
+            }
+
+            i++;
+        }
+        return s;
+    }
+    bool check_vowel(char s, char vowels[]) {
+        for(int i=0; i<10; i++) {
+            if(s == vowels[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+// apG0i4maAs::sA0m4i0Gp0
+
+// (a)pG0i4maAs::sA0m4(i)0Gp0    a i
+
+// ipG0(i)4maAs::s(A)0m4a0Gp0    i A
+        
+// ipG0A4m(a)(A)s::si0m4a0Gp0    a A
+
+// ipG0A4mAas::si0m4a0Gp0
