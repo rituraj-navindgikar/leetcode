966bbda0e8956ca901c3f110432a1225efd0c78e
Rituraj Navindgikar <74851393+rituraj-navindgikar@users.noreply.github.com>
2024-12-01 13:33:14 -0500

    Time: 1065 ms (5.02%) | Memory: 23.8 MB (6.13%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -4,10 +4,15 @@ public:
         int n = nums.size();
         int i=0;
         int j=0;
+        int zero_len = n-1;
+
         for(int i=0; i<n-1; i++) {
             for(int j=0; j<n-1; j++) {
-                if(nums[j] == 0) {
+                if((nums[j] == 0) && (j <= zero_len)) {
                     swap(nums[j], nums[j+1]);
+                }
+                if(j == zero_len){
+                    zero_len--;
                 }
             }
         }
