5a20a5300aaca745e27631f20b338ae7991cfd78
Rituraj Navindgikar <74851393+rituraj-navindgikar@users.noreply.github.com>
2024-12-04 09:26:25 -0500

    Time: 101 ms (42.10%) | Memory: 62.1 MB (44.27%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -6,8 +6,6 @@ public:
         
         sort(nums.begin(), nums.end());
 
-        for(auto x: nums) cout << x << " ";
-        cout << endl;
         int l1 = 0;
         int l2 = n-1;
 
@@ -17,11 +15,8 @@ public:
                 l1++;
                 l2--;
             }
-
-            if(nums[l1] + nums[l2] > k) l2--;
-            if(nums[l1] + nums[l2] < k) l1++;
-            
-            
+            else if(nums[l1] + nums[l2] > k) l2--;
+            else if(nums[l1] + nums[l2] < k) l1++;
         }
 
         return count;
